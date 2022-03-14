98f14b7f89004c4f4b1d2ecaaf2cd427ed11abcc
kkosnik <kelly.kosnik@kellogg.northwestern.edu>
2022-03-14 18:03:06 +0000

    working on showing places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all.where({ user_id: session[:user_id] })
+    @places = Place.all
   end
 
   def show
@@ -13,9 +13,14 @@ class PlacesController < ApplicationController
   end
 
   def create
-    @place = Place.new(params["place"])
-    @place.save
-    redirect_to "/places"
+    if @current_user
+      @place = Place.new(params["place"])
+      @place.user_id = @current_user.id
+      @place.save
+    else
+      flash[:notice] = "Login first."
+    end
+      redirect_to "/places"
   end
 
 end
